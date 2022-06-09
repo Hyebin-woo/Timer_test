76f9702941703cc445dc110c29289969f2ee2618
Hyebin-Woo <dev.hyebin@gmail.com>
2022-06-09 20:51:35 +0900

    add_js

--- script.js
+++ script.js
@@ -1,25 +1,89 @@
-const hour = document.querySelector(".hour");
-const minute = document.querySelector(".minute");
-const second = document.querySelector(".second");
+const eleHour = document.querySelector(".hour");
+const eleMinute = document.querySelector(".minute");
+const eleSecond = document.querySelector(".second");
 
-const startBtn = document.querySelector(".btn-start");
-const resetBtn = document.querySelector(".btn-reset");
+const startBtn = document.querySelector(".btn-start img");
+const resetBtn = document.querySelector(".btn-reset img");
 
-// hour.textContent;
-// minute.textContent;
-// second.textContent;
+let TIME = 0; //타이머 초
+let Timer; // 타이머 : 일초마다 실행되는
+let isPlay = false; // 현재 타이머 실행여부 : startBtn이 뭔지 몰라서
 
-let TIME = 0;
-
-// 타이머 증가 함수
-function updateTimer() {
+// 화면에 보여주기 타이머
+function setTimer(sec) {
+  TIME = sec;
   const hours = Math.floor(TIME / 3600);
-  const checkMinutes = Math.floor(TIME / 60);
   const seconds = TIME % 60;
-  const minutes = checkMinutes % 60;
+  const minutes = Math.floor(TIME / 60) % 60;
 
-  timer.innerText = `${hours < 10 ? `0${hours}` : hours}:${
-    minutes < 10 ? `0${minutes}` : minutes
-  }:${seconds < 10 ? `0${seconds}` : seconds}`;
-  TIME++;
+  eleHour.textContent = hours;
+  eleMinute.textContent = minutes;
+  eleSecond.textContent = seconds;
+}
+//초를 누를때
+eleSecond.addEventListener("click", function () {
+  if (!isPlay) {
+    setTimer(TIME + 10);
+    startBtn.src = "./images/start-blue.png";
+    resetBtn.src = "./images/reset-red.png";
+  }
+});
+
+//분을 누를때
+eleMinute.addEventListener("click", function () {
+  if (!isPlay) {
+    setTimer(TIME + 60);
+    startBtn.src = "./images/start-blue.png";
+    resetBtn.src = "./images/reset-red.png";
+  }
+});
+// 시간을 눌렀을때
+eleHour.addEventListener("click", function () {
+  if (!isPlay) {
+    setTimer(TIME + 3600);
+    startBtn.src = "./images/start-blue.png";
+    resetBtn.src = "./images/reset-red.png";
+  }
+});
+
+// reset누르면 초기화
+resetBtn.addEventListener("click", function () {
+  reset();
+});
+
+function reset() {
+  isPlay = false;
+
+  startBtn.src = "./images/start-default.png";
+  resetBtn.src = "./images/reset-default.png";
+  setTimer(0);
+  clearInterval(Timer);
+}
+
+// startBtn이 총 세번 바뀌니깐 그때마다 조건문 달기
+startBtn.addEventListener("click", function () {
+  if (isPlay == true) {
+    isPlay = false;
+    clearInterval(Timer);
+  } else {
+    if (TIME >= 1) {
+      startBtn.src = "./images/pause.png";
+      goTimer();
+    }
+  }
+});
+
+// countdown 타이머
+function goTimer() {
+  isPlay = true;
+
+  Timer = setInterval(() => {
+    TIME--;
+    if (TIME == 0) {
+      reset();
+    }
+    if (TIME >= 0) {
+      setTimer(TIME); // 화면에 보여주기
+    }
+  }, 1000);
 }
